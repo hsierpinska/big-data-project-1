6702e03d6dd46493e8c61623a178cddfa37aa1b1
hsierpinska <h@h.pl>
2025-11-05 17:19:19 +0100

    mapreduce

--- mapper.py
+++ mapper.py
@@ -1,12 +1,27 @@
-# prosty test 
-# cat input.txt | ./mapper.py | sort | ./reducer.py > output.txt
+# usage cat shrank-data-example.csv python3 mapper.py 
 
+#     line = line.strip()
+
+import csv
 import sys
+# for line in sys.stdin:
+#     line = csv.reader([line]).__next__()
+#     line = line.strip()
+#     if line.startswith("visit_id"):
+#         continue
+#     fields = line.split(",")
+#     hospital_id = fields[1]
+#     date = fields[5]
+#     year = date.split("-")[0]
+#     age = fields[6]
+#     print( "%s,%s\t%s,%s" % (hospital_id, year, 1, age) )
+
 for line in sys.stdin:
-    line = line.strip()
-    words = line.split()
-    for word in words:
-        value = 1
-        print( "%s\t%d" % (word, value) )
-
-
+    line = csv.reader([line]).__next__()
+    if line[0] == "visit_id":
+        continue
+    hospital_id = line[1]
+    date = line[5]
+    year = date.split("-")[0]
+    age = line[6]
+    print( "%s,%s\t%s,%s" % (hospital_id, year, 1, age) )
--- reducer.py
+++ reducer.py
@@ -1,19 +1,35 @@
-#!/usr/bin/env python
 
+# assume the input file is already sorted
 
-import sys
+# filename = 'example-red-input.txt'
+
+#correct output
+# abc123,2023	2,47.5
+# abc123,2024	1,60.0
+# xyz789,2023	2,35.0
+
+# file = open(filename, 'r')
 last_key = None
-sum_value = 0
-for input_line in sys.stdin:
-    input_line = input_line.strip()
-    key, value = input_line.split("\t", 1)
-    value = int(value)
+sum_count = 0
+sum_age = 0
+import sys
+for line in sys.stdin:
+    line = line.strip()
+    key, value = line.split("\t", 1)
+    visit_id = key.split(",")[0]
+    year = int(key.split(",")[1])
+    count = int(value.split(",")[0])
+    age = int(value.split(",")[1])
+    # kluczem jest para visit_id, year
     if last_key == key:
-        sum_value += value
+        sum_count += count
+        sum_age += age
     else:
         if last_key:
-            print( "%s\t%d" % (last_key, sum_value) )
-        sum_value = value
+            avg_age = sum_age / sum_count
+            print( "%s\t%d,%.1f" % (last_key, sum_count, avg_age) )
+        sum_count = count
+        sum_age = age
         last_key = key
-if last_key == key:
-    print( "%s\t%d" % (last_key, sum_value) )
+print( "%s\t%d,%.1f" % (last_key, sum_count, sum_age / sum_count) )
+
