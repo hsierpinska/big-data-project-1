4b1f8ffcc38a0a0837d879a851979ffd5917a782
hsierpinska <106077596+hsierpinska@users.noreply.github.com>
2025-11-08 18:18:29 +0100

    fix hive

--- run_hive.sh
+++ run_hive.sh
@@ -4,7 +4,7 @@ input_dir3=$1
 input_dir4=$2
 output_dir6=$3
 
-beeline -u "jdbc:hive2://localhost:10000/default" -n <twoj_user> -p <twoje_haslo> -f hive.hql \
+beeline -n ${USER} -u jdbc:hive2://localhost:10000/default -f hive.hql \
   --hiveconf input_dir3=$input_dir3 \
   --hiveconf input_dir4=$input_dir4 \
   --hiveconf output_dir6=$output_dir6
